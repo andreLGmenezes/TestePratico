561f91bfe60d0254e4ddf2c1b3b50d2f9309e41b
andreLGmenezes <andregmenezes18@gmail.com>
2024-05-08 15:33:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const addForm = document.getElementById('formulario');
     const itemInput = document.getElementById('adiciona');
     const tableBody = document.getElementById('tabela');
-
     // Dados de exemplo
     let items = [
         { id: 1, name: 'Andre' },
         { id: 2, name: 'Luiz' },
         { id: 3, name: 'Menezes' },
     ];
-
     // Exibe itens na tabela
     function displayItems() {
         tableBody.innerHTML = '';
@@ -23,8 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <button class="delete-btn" data-id="${item.id}">Excluir</button>
               </td>
             `;
-
-            // Adiciona ouvintes de eventos para botões de editar e excluir
+            // botões de editar e excluir
             row.querySelector('.editar-btn').addEventListener('click', (e) => {
                 editItem(e);
             });
@@ -35,7 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
             tableBody.appendChild(row);
         });
     }
-
     // Adiciona um item
     addForm.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -47,23 +43,19 @@ document.addEventListener('DOMContentLoaded', () => {
         displayItems();
         itemInput.value = '';
     });
-
     // Exibe itens iniciais
     displayItems();
 });
-
 // Edita um item
 function editItem(e) {
     const id = e.target.dataset.id;
     const itemToEdit = items.find((item) => item.id === parseInt(id));
     const newName = prompt('Digite o novo nome:');
-
     if (newName) {
         itemToEdit.name = newName;
         displayItems();
     }
 }
-
 // Exclui um item
 function deleteItem(e) {
     const id = e.target.dataset.id;
